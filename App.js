bb1f128feb1b8df8502a38353393ce04b930c74f
Adrien GOGOIS <ad.gogois@gmail.com>
2022-03-02 12:28:26 +0100

    Submit to validate psw and username length and display message

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-// import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { TouchableOpacity, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [validation, setValidation] = useState(false);
+
   const handleSubmit = () => {
-    console.log("SUBMITTED");
+    if (username.length > 5 && password.length > 5) {
+      setValidation(true);
+    }
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput placeholder="Enter your name" />
-      <TextInput placeholder="Enter your password" secureTextEntry />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your name"
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your password"
+        secureTextEntry
+        onChangeText={setPassword}
+      />
       <TouchableOpacity onPress={handleSubmit}>
         <Text>Submit</Text>
       </TouchableOpacity>
-      {/* <StatusBar style="auto" /> */}
+      {validation ? (
+        <View>
+          <Text>Formulaire soumis</Text>
+        </View>
+      ) : (
+        <View>
+          <Text>Entrez un pseudo et un mot de passe valides</Text>
+        </View>
+      )}
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
@@ -30,5 +55,7 @@ const styles = StyleSheet.create({
   input: {
     borderWidth: 1,
     borderRadius: 10,
+    marginBottom: 5,
+    padding: 10,
   },
 });
