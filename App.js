ea1b67b033d810d7f7f8bb0a2d699bcad7b0bee4
Adrien GOGOIS <ad.gogois@gmail.com>
2022-03-02 11:55:55 +0100

    First commit - Structure of form

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-web';
 
 export default function App() {
+
+  const handleClick = () => {
+    console.log("SUBMITTED")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput placeholder='Enter your name' onValueChange={}  />
+      <TextInput placeholder='Enter your password' onValueChange={} secureTextEntry />
+      <TouchableOpacity onPress={handleClick}><Text>Submit</Text></TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
