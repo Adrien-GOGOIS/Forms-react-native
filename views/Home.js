db1848480d2483e4548b78d42bd0bcd00880197f
Adrien GOGOIS <ad.gogois@gmail.com>
2022-03-02 16:05:18 +0100

    Add Logout button in Home.js

--- views/Home.js
+++ views/Home.js
@@ -1,12 +1,25 @@
 import { ScrollView, Text } from "react-native";
 import { TouchableOpacity } from "react-native";
 
-export default function Home() {
+import { useState, useContext } from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { LogContext } from "../App";
+
+export default function Home({ navigation }) {
+  const logState = useContext(LogContext);
+
+  const handleSubmit = () => {
+    logState.setLoggedIn(false);
+    navigation.navigate("Login");
+  };
+
   return (
     <ScrollView>
       <Text>HOME</Text>
-      <TouchableOpacity>
-        <Text></Text>
+      <TouchableOpacity onPress={handleSubmit}>
+        <Text>Logout</Text>
       </TouchableOpacity>
     </ScrollView>
   );
